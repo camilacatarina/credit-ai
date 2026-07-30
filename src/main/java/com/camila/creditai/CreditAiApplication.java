93c32b65de82ee2769b860bfb2c49f2d87da6fd5
camilacatarina <camilacatarina2003@gmail.com>
2026-07-30 19:33:45 -0300

    feat: cria inicialização da aplicação

---
+++ src/main/java/com/camila/creditai/CreditAiApplication.java
@@ -0,0 +1,13 @@
+package com.camila.creditai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreditAiApplication.class, args);
+    }
+
+}
