bfedd2cf62de07ad2810c9711d50fef9a3e2bef9
camilacatarina <camilacatarina2003@gmail.com>
2026-07-30 19:37:36 -0300

    test: adiciona testes iniciais

---
+++ src/test/java/com/camila/creditai/CreditAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camila.creditai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
